8f469b2992a42dfca3863876407ca0d7ae965f64
webserg@gmail.com <webserg@gmail.com>
2016-08-27 22:19:45 +0300

    Initial commit with contributors

---
+++ src/functions.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// Here's a function that takes two `int`s and returns
+// their sum as an `int`.
+func plus(a int, b int) int {
+
+	// Go requires explicit returns, i.e. it won't
+	// automatically return the value of the last
+	// expression.
+	return a + b
+}
+
+func main() {
+
+	// Call a function just as you'd expect, with
+	// `name(args)`.
+	res := plus(1, 2)
+	fmt.Println("1+2 =", res)
+}
+
